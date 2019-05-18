e5339c2fac95fd9f01626774f7a5b14acf1e80d0
amegha <aeghm02@gmail.com>
2019-05-18 18:01:13 +0530

    testing

--- HelpDeskMVC/src/com/xworkz/helpdesk/DAO/IssueTicketDAO.java
+++ HelpDeskMVC/src/com/xworkz/helpdesk/DAO/IssueTicketDAO.java
@@ -2,7 +2,8 @@ package com.xworkz.helpdesk.DAO;
 
 import com.xworkz.helpdesk.DTO.IssueTicketDTO;
 import com.xworkz.helpdesk.entity.IssueTicketEntity;
-
+//changing
+//local branch changes
 public interface IssueTicketDAO {
 	public void save(IssueTicketEntity entity/*IssueTicketDTO issueTicketDTO*/);
 
